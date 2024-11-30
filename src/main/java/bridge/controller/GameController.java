3d0af94b4b44b490adc5fa4a1c50c47b50df1f74
seongmin <0411tjdals34@gmail.com>
2024-11-30 16:37:02 +0900

    feat: 다리 건너기 결과의 출력 메서드를 호출

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -4,6 +4,7 @@ import bridge.BridgeGame;
 import bridge.model.Bridge;
 import bridge.model.BridgeMaker;
 import bridge.model.BridgeRandomNumberGenerator;
+import bridge.model.User;
 import bridge.view.InputView;
 import bridge.view.OutputView;
 
@@ -24,13 +25,18 @@ public class GameController {
         Bridge bridge = new Bridge(makeBridge, bridgeSize);
 
         int location = 0;
+        User user = new User();
         while (true) {
+
             OutputView.printMovingSpaceMessage();
             boolean isMoving = bridgeGame.move(bridge, location, InputView.readMoving());
             if (isMoving) {
                 location++;
+                printUserRoute(user, bridge);
             }
+
             if (!isMoving) {
+                printUserRoute(user, bridge);
                 OutputView.printGameRestartMessage();
                 String restartingInput = InputView.readGameCommand();
                 boolean isRestart = bridgeGame.retry(restartingInput);
@@ -42,5 +48,25 @@ public class GameController {
                 }
             }
         }
+    }
+
+    private void printUserRoute(User user, Bridge bridge) {
+        List<String> upRoute = user.getUpRoute();
+        List<String> downRoute = user.getDownRoute();
+        OutputView.printMapStart();
+        for (int i = 0; i < upRoute.size(); i++) {
+            OutputView.printMap(upRoute.get(i));
+            if (i != upRoute.size()-1) {
+                OutputView.printMapStep();
+            }
+        }
+        for (int i = 0; i < downRoute.size(); i++) {
+            OutputView.printMap(downRoute.get(i));
+            if (i != downRoute.size()-1) {
+                OutputView.printMapStep();
+            }
+        }
+        OutputView.printMapEnd();
+
     }
 }
