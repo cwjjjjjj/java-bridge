9c9fa20fc045ad7ee8bcae649474d66bfb0fb1dc
seongmin <0411tjdals34@gmail.com>
2024-11-30 15:15:28 +0900

    feat: 출력 메세지 상수화, 출력 메서드 생성

--- src/main/java/bridge/controller/GameController.java
+++ src/main/java/bridge/controller/GameController.java
@@ -7,10 +7,15 @@ import bridge.view.InputView;
 
 import java.util.List;
 
+import static bridge.view.OutputView.printBridgeSizeMessage;
+import static bridge.view.OutputView.printStartMessage;
+
 public class GameController {
     BridgeMaker bridgeMaker = new BridgeMaker(new BridgeRandomNumberGenerator());
 
     public void run() {
+        printStartMessage();
+        printBridgeSizeMessage();
         int bridgeSize = InputView.readBridgeSize();
         List<String> makeBridge = bridgeMaker.makeBridge(bridgeSize);
         Bridge bridge = new Bridge(makeBridge, bridgeSize);
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -4,7 +4,21 @@ package bridge.view;
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private final static String START_MESSAGE = "다리 건너기 게임을 시작합니다.\n";
+    private final static String BRIDGE_SIZE_MESSAGE = "다리의 길이를 입력해주세요.";
+    private final static String MOVING_SPACE_MESSAGE = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private final static String GAME_RESTART_MESSAGE = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
+    private final static String FINAL_RESULT_MESSAGE = "최종 게임 결과";
+    private final static String IS_SUCCEED_MESSAGE = "게임 성공 여부: ";
+    private final static String NUMBER_OF_TRIAL_MESSAGE = "총 시도한 횟수: ";
 
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public static void printBridgeSizeMessage() {
+        System.out.println(BRIDGE_SIZE_MESSAGE);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
